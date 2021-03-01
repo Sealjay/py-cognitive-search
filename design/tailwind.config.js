5de3cdfd22b62f6bbc4cde279ba8e7aaa2279b71
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-03-01 21:25:20 +0000

    added basic tailwind build

---
+++ design/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+      enabled: true,
+      content: [
+          '*.html'
+      ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
